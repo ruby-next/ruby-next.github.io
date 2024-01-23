59899dd0f4a4cded3c2b7289dbebdde4fcc8318d
Vladimir Dementyev <palkan@hey.com>
2024-01-22 21:56:07 -0800

    feat: implement run + puts/p support

--- src/app.js
+++ src/app.js
@@ -16,49 +16,153 @@ puts greet.call(hello: 'martian')
 
 const DEFAULT_PREVIEW = `# Here you will see the transpiled source code.`;
 
+const CONFIG = `# Here you can define custom source rewriters.`;
+
+const OUTPUT = "// Here will be the output of your program";
+
+const THEMES = {
+  light: "vs",
+  dark: "vs-dark",
+};
+
 export default class App {
   constructor(el, vm, monaco) {
     this.el = el;
     this.vm = vm;
     this.monaco = monaco;
+
+    this.onSelectEditor = this.onSelectEditor.bind(this);
   }
 
   bootstrap() {
-    this.currentEditor = this.initEditor(
-      this.el.querySelector('[target="editor"]'),
+    const theme = window.matchMedia?.("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+
+    this.monaco.editor.setTheme(THEMES[theme]);
+
+    this.codeEditor = this.initEditor(
+      this.el.querySelector("#codeEditor"),
       DEFAULT_SOURCE
     );
 
+    this.configEditor = this.initEditor(
+      this.el.querySelector("#configEditor"),
+      CONFIG
+    );
+
     this.previewEditor = this.initEditor(
-      this.el.querySelector('[target="preview"]'),
+      this.el.querySelector("#previewEditor"),
       DEFAULT_PREVIEW,
       {
         readOnly: true,
       }
     );
 
+    this.outputEditor = this.initEditor(
+      this.el.querySelector("#outputEditor"),
+      OUTPUT,
+      {
+        readOnly: true,
+        language: "shell",
+        lineNumbers: "off",
+      }
+    );
+
     this.el
       .querySelector('[target="transpile-btn"]')
       .addEventListener("click", () => {
-        const code = this.currentEditor.getValue();
-        const result = this.vm
-          .eval("RubyNext::Language.transform(%q(" + code + "), using: false)")
-          .toString();
+        const result = this.transpile(this.codeEditor.getValue());
 
         this.previewEditor.setValue(result);
+        this.showEditor("previewEditor");
       });
+
+    this.el
+      .querySelector('[target="run-btn"]')
+      .addEventListener("click", () => {
+        const newSource = this.transpile(this.codeEditor.getValue());
+        this.previewEditor.setValue(newSource);
+
+        const result = this.execute(newSource);
+        let output = window.$puts.flush();
+
+        if (result) output += "\n\n> " + result;
+
+        this.outputEditor.setValue(output);
+
+        this.showEditor("outputEditor");
+      });
+
+    this.el.addEventListener("change", this.onSelectEditor);
+
+    this.setCurrentVersion();
+  }
+
+  transpile(code) {
+    const result = this.vm
+      .eval("RubyNext.transform(%q(" + code + "))")
+      .toString();
+
+    return result;
+  }
+
+  execute(source) {
+    try {
+      return this.vm.eval(source).toString();
+    } catch (e) {
+      console.error(e);
+      return e.message;
+    }
+  }
+
+  async setCurrentVersion() {
+    const versionContainer = document.getElementById("currentVersion");
+    if (!versionContainer) return;
+
+    const version = this.execute("RUBY_VERSION + '-' + RUBY_PLATFORM");
+
+    versionContainer.innerText = version;
   }
 
   initEditor(target, value, opts = {}) {
     return this.monaco.editor.create(target, {
       value,
       language: "ruby",
-      theme: "vs-dark",
       automaticLayout: true,
       minimap: {
         enabled: false,
       },
       ...opts,
     });
+  }
+
+  showEditor(editorName) {
+    const editor = this.el.querySelector(`#${editorName}`);
+    if (!editor) return;
+
+    const containerId = editor.dataset.pane;
+    if (!containerId) return;
+
+    const container = this.el.querySelector(`#${containerId}`);
+    if (!container) return;
+
+    // Hide previous editor
+    container
+      .querySelector("[data-pane]:not(.hidden)")
+      ?.classList.add("hidden");
+
+    // Make sure the correct radio is checked
+    const radio = container.querySelector(`input[value="${editorName}"]`);
+    if (radio) radio.checked = true;
+
+    // Show the editor
+    editor.classList.remove("hidden");
+  }
+
+  onSelectEditor(e) {
+    if (!e.target.value) return;
+
+    this.showEditor(e.target.value);
   }
 }
--- src/vm.js
+++ src/vm.js
@@ -1,9 +1,20 @@
 import { DefaultRubyVM } from "@ruby/wasm-wasi/dist/browser";
+import { consolePrinter } from "@ruby/wasm-wasi";
 import ruby from "./ruby.wasm";
 
 export default async function initVM() {
   const module = await ruby();
+
+  const output = [];
+  const originalLog = console.log;
+  window.$puts = function (val) {
+    originalLog(val);
+    output.push(val);
+  };
+
   const { vm } = await DefaultRubyVM(module);
+
+  window.$puts.flush = () => output.splice(0, output.length).join("\n");
 
   vm.eval(`
     require "/bundle/setup"
@@ -11,9 +22,33 @@ export default async function initVM() {
     # Make gem no-op
     define_singleton_method(:gem) { |*| nil }
 
+    require "js"
+
+    module Kernel
+      def puts(val)
+        JS.eval("window.$puts('#{val.inspect}')")
+        nil
+      end
+
+      def p(val)
+        JS.eval("window.$puts('#{val}')")
+        nil
+      end
+    end
+
     require "ruby-next/language"
     require "ruby-next/language/rewriters/edge"
     require "ruby-next/language/rewriters/proposed"
+
+    module RubyNext
+      def self.transform(code, version: RUBY_VERSION, using: false)
+        options = {using:}
+        options[:rewriters] = Language.rewriters.select { |rw| rw.unsupported_version?(version) }
+
+        source = Language.transform(code, **options)
+        source += "\n # Transformed with RubyNext v#{RubyNext::VERSION} for Ruby #{version}"
+      end
+    end
   `);
 
   return vm;
